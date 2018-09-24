f5ea2bf050189254b42455022b6e47199bd7acb8
Henrique Krausburg Corrêa <henrique.krausburg.correa@gmail.com>
2018-09-24 19:38:05 -0300

    Completed sync validation. Updated TODO comments

--- T1/tb_rcv_fsm.vhd
+++ T1/tb_rcv_fsm.vhd
@@ -1,8 +1,23 @@
 --------------------------------------------------
 -- File:    tb_rcv_fsm.vhd
--- Author:  Henrique Krausburg Corrêa <henrique.krausburg.correa@gmail.com>
+-- Author:  Henrique Krausburg Corrï¿½a <henrique.krausburg.correa@gmail.com>
 -- Author:  Giuseppe Generoso
 --------------------------------------------------
+
+--TODO: Testes que faltam ser implementados:
+--data_pl_out (passivo):	Sï¿½ pode ocorrer uma mudanï¿½a no barramento se a flag de data_en_out estiver ativa ou se o reset foi acionado;
+--data_pl_out (ativo): 	Escrever uma rajada de dados serial (5x payload) e verificar se estï¿½ na saï¿½da;
+--						NOTA: O requisito nï¿½o deixa claro quanto tempo leva para o dado estar disponï¿½vel na saï¿½da paralela. Considerar uma tolerï¿½ncia de 7 ciclos de clock.
+--						DICA: mandar pelo menos 2 bytes de payload replicados
+--data_en_out (passivo): 	analisar transiï¿½ï¿½es vï¿½lidas do sinal conforme requisito
+--							NOTA: nï¿½o esquecer de testar se o sinal Nï¿½O ficou mais de um ciclo em nï¿½vel alto (talvez fazer uma fila de histï¿½rico de 2 bits).
+--reset (ativo):	verificar se REALMENTE funciona apenas em borda de subida (tentar ressetar em borda de descida);
+--					reiniciar apï¿½s sincronizado (flag de alinhamento ï¿½ desativada);
+--					reiniciar antes da sincronizaï¿½ï¿½o (verificar se alinhamento ï¿½ ativo depois de 3x palavras)
+--sync_out(ativo):	Para borda de descida 1 -> 0, verificar se a transmissï¿½o de dados serial foi interrompida (exemplo: s_data_sr_in <= 'Z')
+--					Para borda de descida 1 -> 0, verificar se realmente ocorreu falha no alinhamento
+--
+--POR FIM: Test coverage passando 100%
 
 library ieee;
     use ieee.std_logic_1164.all;
@@ -57,28 +72,42 @@ begin
 	test: process
 	begin
 		wait until s_rst_in = '0';
+		--TODO: overwrite these sample procedures for formal test procedures
+		aux_sync_device(s_clk, s_data_sr_in);
 		loop
-			aux_generate_alignment(s_clk, s_data_sr_in);
 			aux_generate_dummy_payload(s_clk, s_data_sr_in);
+			aux_generate_alignment(s_clk, s_data_sr_in);
 		end loop;
+		s_finishTest <= '1';
 	end process test;
 	
-	--Procedure to validate 
-	-- 1) comparar com o previous sync
-	-- 2) se for borda de subida: ultimas 3 palavras de alinhamento devem ser validas
-	-- 3) se for borda de descida: observar falha no alinhamento // erro logo atras
 	sync_validation: process(s_sync_out)
 	constant alignment: std_logic_vector (7 downto 0) := "10100101";
+	variable sync_validation: boolean := false;
 	begin
 		if (s_sync_out = '1' and s_previous_sync_out = '0') then
+			--Validate 0 -> 1 transition
 			assert (s_serial_queue(7 downto 0) = alignment and
 				s_serial_queue(55 downto 48) = alignment and
 				s_serial_queue(103 downto 96) = alignment)
 			report "Test alignment validation: Invalid sync transition 0 => 1: " --& hstr(s_serial_queue)-- & " " & hstr(s_serial_queue(20 downto 13)) & " " & hstr(s_serial_queue(33 downto 26))
 			severity error;
+		elsif(s_sync_out = '0' and s_previous_sync_out = '1') then
+			--Validate 1 -> 0 transition
+			
+			--Check if reset button has been pressed
+			sync_validation := sync_validation or (s_rst_in = '1');
+
+			--Check if 1 -> 0 reason was an invalid std_logic value
+			sync_validation := sync_validation or (s_data_sr_in /= '1' and s_data_sr_in /= '0');
+
+			--Check if 1 -> 0 reason was caused by wrong alignment word
+			sync_validation := sync_validation or aux_is_alignment_broken(s_serial_queue(55 downto 0));
+
+			--Validate 1 -> 0 transition
+			assert (sync_validation = true) report "Test alignment validation: Invalid sync transition 1 => 0" severity error;
 		end if;
 	end process sync_validation;
-
 
 	--Instantiate CUV
 	cuv: entity work.rcv_fsm
